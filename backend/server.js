ebaf71a921582b40c97b6b121956815713eb4502
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2026-05-01 20:57:34 +0530

    fic

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const taskRoutes = require('./routes/taskRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
